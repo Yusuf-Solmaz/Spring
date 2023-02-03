b1568cf6e6ae9c50277d1176e0a501745ba477e6
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2023-02-04 00:31:24 +0300

    Update Random Method

--- src/main/java/com/example/tomcat10/springConfAnnotations/constuctorInjection/MyFortune.java
+++ src/main/java/com/example/tomcat10/springConfAnnotations/constuctorInjection/MyFortune.java
@@ -2,27 +2,18 @@ package com.example.tomcat10.springConfAnnotations.constuctorInjection;
 
 import org.springframework.stereotype.Component;
 
+import java.util.Random;
+
 @Component
 public class MyFortune implements FortuneService {
 
     public String[] fortune={"You will be successful in your work.","You will be unusually successful in business.","You will enjoy good health."};
 
-    public int randomVar= (int) (Math.random()*2)+1;
-
-    public String getRandomFortune(){
-        if (randomVar>=0 && randomVar<1){
-            return fortune[0];
-        }
-        else if (randomVar>=1 && randomVar<2){
-            return fortune[1];
-        }
-        else {
-        return fortune[2];
-    }
-    }
+    private Random random = new Random();
+    int myRandomIndex = random.nextInt(fortune.length);
 
     @Override
     public String getFortune() {
-        return getRandomFortune();
+        return fortune[myRandomIndex];
     }
 }
