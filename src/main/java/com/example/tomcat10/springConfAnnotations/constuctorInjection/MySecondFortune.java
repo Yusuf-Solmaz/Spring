16c11251c0e0d182fe29e7c2ad6c087094eda401
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2023-02-04 15:07:55 +0300

    Random Fortune Service With Reading a txt File

--- src/main/java/com/example/tomcat10/springConfAnnotations/constuctorInjection/MySecondFortune.java
+++ src/main/java/com/example/tomcat10/springConfAnnotations/constuctorInjection/MySecondFortune.java
@@ -1,11 +1,41 @@
 package com.example.tomcat10.springConfAnnotations.constuctorInjection;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 @Component()
 public class MySecondFortune implements FortuneService{
+
+    private String textPath = "C:/Users/Yusuf/Desktop/Spring/src/main/resources/fortunes.txt";
+    private List<String> fortune = new ArrayList<String>();
+    private Random random = new Random();
+
+
+    public MySecondFortune() throws IOException {
+
+        File file = new File(textPath);
+        FileReader fr = new FileReader(file);
+        BufferedReader br = new BufferedReader(fr);
+
+        String line;
+
+        while ((line = br.readLine()) != null) {
+            fortune.add(line);
+        }
+
+    }
+
     @Override
     public String getFortune() {
-        return "This is a second fortune and you will be lucky today.";
+
+        int index = random.nextInt(fortune.size());
+        String tempFortune = fortune.get(index);
+
+        return tempFortune;
     }
 }
